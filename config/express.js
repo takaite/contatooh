be440804af82ec3837fc2df6a612db9fa45f516c
Takaite Takehara <takaite@brasilafora.com.br>
2016-09-15 09:01:18 -0300

    Starting template

--- config/express.js
+++ config/express.js
@@ -1,6 +1,18 @@
 var express = require('express');
 
+// This function will be called as 'express'();
 module.exports = function() {
   var app = express();
+
+  // Environment vars
+  app.set ('port', 3000);
+
+  // middleware
+  app.use (express.static('./public'));
+
+  // configure view engine
+  app.set ('view engine', 'ejs');
+  app.set ('view', './app/views');
+
   return app;
 };
